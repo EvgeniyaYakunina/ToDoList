840070839f2ee6320a919e07cd4d69c4ba4d7e48
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-06-14 21:26:36 +0300

    api header

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,10 @@ import axios, {AxiosResponse} from "axios";
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true
-    // headers: {
-    //     'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
-    // }
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'dda441b2-5c1f-4026-aae6-ffbccdaecd79'
+    }
 })
 
 // api
